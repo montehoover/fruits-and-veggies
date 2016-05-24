c50275edca6af07c8028ccff169d4c72f13bc7f8
montehoover <monte.b.hoover@gmail.com>
2016-05-24 08:58:15 -0700

    Basic angular functionality.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,5 +1,15 @@
 /* setup your angular app here */
 var veggieApp = angular.module('VeggieApp', []);
+veggieApp.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.name = "bill murray";
+
+  $scope.happy = false;
+
+  $scope.toggleHappy = function() {
+    $scope.happy = !$scope.happy
+  }
+
+}]);
 
 
 //debug stuff to show the app is loading and fruit / veggies are available
